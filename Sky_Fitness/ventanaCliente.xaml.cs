07cf51621dbde6cb883c6be07d3484da527474c4
Abel <enriquezun3@gmail.com>
2019-07-31 23:47:50 -0600

    Agregar Cliente funcional, falta corregir errores pero ya se guarda en la base de datos

--- Sky_Fitness/ventanaCliente.xaml.cs
+++ Sky_Fitness/ventanaCliente.xaml.cs
@@ -27,31 +27,73 @@ namespace Sky_Fitness
         public ventanaCliente()
         {
             InitializeComponent();
-            SqlConnection conexion = new SqlConnection("Data Source = (local); Initial Catalog = SistemaDeEstacionamiento; Integrated Security = True");
-            dataContextSky = new SkyFitnessBDDataContext(); 
+            SqlConnection conexion = new SqlConnection("Data Source = (local); Initial Catalog = Sky_FitnessDB; Integrated Security = True");
+            dataContextSky = new SkyFitnessBDDataContext(conexion); 
         }
 
         private void InsertarCliente()
         {
-            Cliente cliente = new Cliente();
+            try
+            { 
+                Cliente cliente = new Cliente();
 
-            cliente.numeroIdentidad = txtIdentidad.Text;
-            cliente.nombre = txtNombre.Text;
-            cliente.apellido = txtApellido.Text;
-            cliente.telefono = txtTelefono.Text;
-            cliente.direccion = txtDireccion.Text;
-            cliente.correoElectronico = txtCorreo.Text;
-            cliente.fechaNacimiento = dpFechaNacimiento.DisplayDate;
-            cliente.razon = cbRIngreso.SelectedValue.ToString();
-            cliente.edad = Convert.ToInt32(txtEdad);
-            cliente.peso = Convert.ToDecimal(txtPeso);
-            cliente.estatura = Convert.ToDecimal(txtEstatura);
-            cliente.talla = Convert.ToDecimal(txtTalla);
-            cliente.IMC = Convert.ToDecimal(txtIMC);
-            if (rbMasculino.IsEnabled)
-                cliente.sexo = "Masculino";
-            else if (rbFemenino.IsEnabled)
-                cliente.sexo = "Femenino";
+                cliente.numeroIdentidad = txtIdentidad.Text;
+                cliente.nombre = txtNombre.Text;
+                cliente.apellido = txtApellido.Text;
+                cliente.telefono = txtTelefono.Text;
+                cliente.direccion = txtDireccion.Text;
+                cliente.correoElectronico = txtCorreo.Text;
+                cliente.fechaNacimiento = Convert.ToDateTime(dpFechaNacimiento.SelectedDate);
+                cliente.razon = cmbRIngreso.Text;
+                cliente.edad = Convert.ToInt32(txtEdad.Text);
+                cliente.peso = Convert.ToDecimal(txtPeso.Text);
+                cliente.estatura = Convert.ToDecimal(txtEstatura.Text);
+                cliente.talla = Convert.ToDecimal(txtTalla.Text);
+                cliente.IMC = Convert.ToDecimal(txtIMC.Text);
+                cliente.fechaCreacion = DateTime.Now;
+                if (rbMasculino.IsChecked == true)
+                    cliente.sexo = "Masculino";
+                else if (rbFemenino.IsChecked == true)
+                    cliente.sexo = "Femenino";
+                dataContextSky.Cliente.InsertOnSubmit(cliente);
+                dataContextSky.SubmitChanges();
+
+                MessageBox.Show("Se ha agregado con exito");
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void BtnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            InsertarCliente();
+            
+        }
+
+        private void BtnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            LimpiarDatos();
+        }
+        private void LimpiarDatos()
+        {
+            txtIdentidad.Clear();
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtTelefono.Clear();
+            txtDireccion.Clear();
+            txtCorreo.Clear();
+            dpFechaNacimiento.SelectedDate = DateTime.Now;
+            cmbRIngreso.SelectedIndex = -1;
+            rbMasculino.IsChecked = false;
+            rbFemenino.IsChecked = false;
+            txtEdad.Clear();
+            txtPeso.Clear();
+            txtEstatura.Clear();
+            txtTalla.Clear();
+            txtIMC.Clear();
         }
     }
 }
